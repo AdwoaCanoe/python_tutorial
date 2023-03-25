60e9beb4ec41a9e3d0e4ebeb1017b609ee7b160b
AdwoaCanoe <99411206+AdwoaCanoe@users.noreply.github.com>
2023-03-25 15:55:28 +0000

    Add density.py

---
+++ python_scripts/density.py
@@ -0,0 +1,9 @@
+#Density of a substance
+
+mass = float(input("Enter mass of substance in grams: "))
+
+volume = float(input("Enter volume of substance in centimeter: "))
+
+density = mass * volume 
+
+print("Density of substance is :", density,"g/cm3")
