ecbb41c2c971ca95b672f8059b2320cf0544bb6d
AdwoaCanoe <99411206+AdwoaCanoe@users.noreply.github.com>
2023-03-25 15:57:11 +0000

    Add velocity.py

---
+++ python_scripts/velocity.py
@@ -0,0 +1,13 @@
+# Velocity
+
+initial_displacement = float(input("Enter initial displacement in meters: "))
+
+final_displacement = float(input("Enter final displacement in meters: "))
+
+initial_time = float(input("Enter initial time in seconds : "))
+
+final_time = float(input("Enter initial displacement in seconds: "))
+
+velocity = (final_displacement - initial_displacement) / (final_time - initial_time)
+
+print("velocity is: ", velocity,"m/s")
