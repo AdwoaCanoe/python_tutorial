b2a87a7b586d52c03aca712bc0f7faab7cfa5b56
AdwoaCanoe <99411206+AdwoaCanoe@users.noreply.github.com>
2023-03-25 15:55:47 +0000

    Add force.py

---
+++ python_scripts/force.py
@@ -0,0 +1,9 @@
+#Force of a moving object
+
+object_mass = float(input("Enter mass of object : "))
+
+acceleration = float(input("Enter acceleration of object: "))
+
+force = mass * acceleration 
+
+print("force acting on object is: " ,force,"N")
