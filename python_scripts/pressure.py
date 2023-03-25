546f5dcf2f1a200c31195ba6372f1bace1dbc7a3
AdwoaCanoe <99411206+AdwoaCanoe@users.noreply.github.com>
2023-03-25 15:56:58 +0000

    Add pressure.py

---
+++ python_scripts/pressure.py
@@ -0,0 +1,9 @@
+#Pressure
+
+object_force = float(input("Enter force of object : "))
+
+object_area = float(input("Enter area of object: "))
+
+pressure = object_force * object_area
+
+print("pressure is : ", pressure , "pascal")
